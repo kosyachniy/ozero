5cfd663e8abf374df564dc765b8064297b935858
kutikova2016 <kutikova2016@mail.ru>
2017-07-25 11:32:10 +0500

    'Can_remember_names_and_dates'

--- main.py
+++ main.py
@@ -1,47 +1,49 @@
 from func import *
 from lake import *
 
-mess(144520879, 'Привет, я робот-помошник Тетрис-4.' + '\n'
-                'Не хочешь зарегестрироваться на игру Озеро?' + '\n'
-                'Напиши "Помощь" и узнаешь о чём со мной можно поговорить')
+#mess(144520879, 'Привет, я робот-помошник Тетрис-4.' + '\n'
+#                'Не хочешь зарегестрироваться на игру Озеро?' + '\n'
+#                'Напиши "Помощь" и узнаешь о чём со мной можно поговорить')
 #mess(135524838, 'Слыш, ты!')
 
-d = {'t': 100}
-print(d)
-print(d['t'])
 
 values = {'out':0, 'count':10, 'time_offset':3600}
 
-dict_of_dilogs = {'144520879': ('Вано', 1500828375)} #словарь кортежей. id: ("Способ обращения"(str), "Дата последнего сообщения"(int))
-put_obj(dict_of_dilogs, 'dict_of_dilogs')
-dict_of_dilogs = loads(get_obj('dict_of_dilogs'))
+#dict_of_dilogs = {'144520879': ('Вано', 1500828375)} #словарь кортежей. id: ("Способ обращения"(str), "Дата последнего сообщения"(int))
+dict_of_dilogs = {}
+#put_obj(dict_of_dilogs, 'dict_of_dilogs')
+dict_of_dilogs = get_obj('dict_of_dilogs')
 
-
-print(dict_of_dilogs)
-print(type(dict_of_dilogs))
-dict_of_dilogs['t'] = 1
-print(dict_of_dilogs['t'])
 kol_lakes = 0
 for i in range(100):
     res = vk.method('messages.get', values)
-    #print(res)
-    if res['items']:
-        values['last_message_id'] = res['items'][0]['id']
+    print(res)
+    #if res['items']:
+    #    values['last_message_id'] = res['items'][0]['id']
     for item in res['items']:
         id = item['user_id']
         print(str(id))
         print(dict_of_dilogs[str(id)])
-        if not(str(id) in dict_of_dilogs):
+
+        if not(str(id) in dict_of_dilogs): #если с этим человеком мы ещё не разговривали, добавляем его в словарь
             dict_of_dilogs[str(id)] = ('Человек', item['date'])
-        elif (dict_of_dilogs[str(id)][1] > item['date']): #отвечаем только на последние сообщения данного пользователя
+            put_obj(dict_of_dilogs, 'dict_of_dilogs')
+        elif (dict_of_dilogs[str(id)][1] >= item['date']): #отвечаем только на последние сообщения данного пользователя
             continue
         else:
-            dict_of_dilogs[id][1] = item['date']
+            dict_of_dilogs[str(id)][1] = item['date']
+            put_obj(dict_of_dilogs, 'dict_of_dilogs')
         words = get_words(item['body'])
+        print("Words:= "words)
         if words[0] in privetstvie:
-            mess(id, "И тебе привет " + dict_of_dilogs[id] + ' :-)')
+            mess(id, "И тебе привет, " + dict_of_dilogs[str(id)][0] + ' :-)')
             words.remove(words[0])
 
+        if len(words) > 2 and (words[0] == 'Называй' and words[1] == 'Меня'):
+            dict_of_dilogs[str(id)][0] = words[2]
+            put_obj(dict_of_dilogs, 'dict_of_dilogs')
+            mess(id, 'Хорошо, товарищ ' + words[2])
+            continue
         if len(words) == 1 and words[0] in slovarni_zapas_oneword:
             mess(id, slovarni_zapas_oneword[words[0]])
             continue
